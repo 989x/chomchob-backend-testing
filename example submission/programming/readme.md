222f074034b54728f52244aeaf3fe54fd457fc0a
SinsamutQ <99growsup@gmail.com>
2023-11-13 15:04:46 +0700

    Connected mariadb with sequelize and use docker

--- example submission/programming/readme.md
+++ example submission/programming/readme.md
@@ -1,12 +1,16 @@
-### wallet-api
+## Document for wallet-api
 
-start
+### Start
 
 ```bash
+# docker
+docker-compose up -d
+
+# express
 pnpm dev
 ```
 
-install
+### Install
 
 ```bash
 pnpm init
@@ -14,4 +18,8 @@ pnpm init
 # express
 pnpm i express
 pnpm i -D @types/node @types/express typescript eslint nodemon ts-node dotenv
+
+# maria
+pnpm i mariadb
+pnpm i mysql2 sequelize
 ```
--- example submission/programming/wallet-api/server.ts
+++ example submission/programming/wallet-api/server.ts
@@ -1,13 +1,30 @@
 import express from "express";
 import { Request, Response } from "express";
+import sequelize from "./config/db-sequelize";
 
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+
+async function startServer() {
+  try {
+    await sequelize.sync()
+    console.log("Database synchronized");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    })
+  } catch (err) {
+    console.error("Error syncing the database:", err);
+  }
+}
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/test", (req: Request, res: Response) => {
+  res.status(200).send("Ok");
 });
+
+startServer();
